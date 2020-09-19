6db41cb70a42f770605099e9b1b1326295f99b70
TheFallenCol <germansilva1944@outlook.com>
2020-09-18 23:10:30 -0500

    Cambio de nombre de API - Controlador Funcionando

--- JWTokenAuthentication/AuthenticationAPI/Startup.cs
+++ JWTokenAuthentication/AuthenticationAPI/Startup.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using UnitOfWork;
 
-namespace WebApi
+namespace AuthenticationAPI
 {
     public class Startup
     {
@@ -21,7 +28,7 @@ namespace WebApi
         {
             services.AddSingleton<IUnitOfWork>(option => new DataAccess.UnitOfWork(
                 Configuration.GetConnectionString("AutenthicationDB")
-            ));
+                ));
             services.AddControllers();
         }
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ En este repositorio se encuentra la interfaz gráfica del proyecto y el microser
 
 Interfaz gráfica el proyecto de Arquitectura Empresarial de Software de la universidad Javeriana
 
+Image by <a href="https://pixabay.com/users/Yuri_B-2216431/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4266512">Yuri_B</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4266512">Pixabay</a>
