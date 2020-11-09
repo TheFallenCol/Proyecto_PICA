540c18043a9082d013873c1efcd36034bec3cd45
TheFallenCol <germansilva1944@outlook.com>
2020-11-08 19:19:07 -0500

    Modulos adicionados

--- WebApplication-SPA/src/app/app.module.ts
+++ WebApplication-SPA/src/app/app.module.ts
@@ -1,18 +1,44 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './common/not-found/not-found.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent,    
+    NotFoundComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatSidenavModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:HomeComponent
+      },
+      {
+        path:'login',
+        component:HomeComponent
+      },
+      {
+        path:'**',
+        component:NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
