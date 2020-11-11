d069a064ff3004aca7b2bc1f1efc1686d19301f2
TheFallenCol <germansilva1944@hotmail.com>
2020-11-11 16:26:40 -0500

    Quitar llamados innecesarios

--- WebApplication-SPA/src/app/app-routing.module.ts
+++ WebApplication-SPA/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NotFoundComponent } from './common/not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes : Routes = [
-  { path:'', component:HomeComponent },  
+  { path:'', component:HomeComponent },
   { path:'**', component:NotFoundComponent }
 ];
 
