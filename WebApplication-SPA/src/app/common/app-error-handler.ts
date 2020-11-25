54628f495f9ea88d780bb15c845c6555d0aa783d
TheFallenCol <germansilva1944@outlook.com>
2020-11-24 23:08:14 -0500

    remove console.log sentece

--- WebApplication-SPA/src/app/common/app-error-handler.ts
+++ WebApplication-SPA/src/app/common/app-error-handler.ts
@@ -4,6 +4,5 @@ export class AppErrorHandler implements ErrorHandler{
     handleError(error){
         //Codigo para almacenar trazas de errores o algo que pase en estos casos
         alert('Unexpected error ocurred');
-        console.log(error);
     }
 }
