ca411922e131cd117de15519e92d96d4f95bc28c
TheFallenCol <germansilva1944@hotmail.com>
2020-11-25 18:00:15 -0500

    Add modules

--- WebApplication-SPA/src/app/eventos/eventos.module.ts
+++ WebApplication-SPA/src/app/eventos/eventos.module.ts
@@ -6,18 +6,19 @@ import { CommonModule } from '@angular/common';
 import { EventosRoutingModule } from './eventos-routing.module';
 import { ConsultarEventosComponent } from './consultar-eventos/consultar-eventos.component';
 import { MatStepperModule } from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatInputModule} from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule} from '@angular/material/divider';
 import { CardEventComponent } from './card-event/card-event.component';
+import { VuelosModule } from '../vuelos/vuelos.module';
+
 
 @NgModule({
   declarations: [
@@ -25,9 +26,9 @@ import { CardEventComponent } from './card-event/card-event.component';
     CardEventComponent
   ],
   imports: [
+    VuelosModule,
     MatCardModule,
     MatDividerModule,
-    MatTabsModule,
     MatNativeDateModule,
     MatIconModule,
     MatButtonModule,
