82b00de08abfca43d23057b1d17369327cc84f3d
TheFallenCol <germansilva1944@outlook.com>
2020-11-28 17:21:41 -0500

    Change from alert to console log

--- WebApplication-SPA/src/app/common/app-error-handler.ts
+++ WebApplication-SPA/src/app/common/app-error-handler.ts
@@ -3,6 +3,6 @@ import { ErrorHandler } from '@angular/core';
 export class AppErrorHandler implements ErrorHandler{
     handleError(error){
         //Codigo para almacenar trazas de errores o algo que pase en estos casos
-        alert('Unexpected error ocurred');
+        console.log('Unexpected error ocurred', error);
     }
 }
