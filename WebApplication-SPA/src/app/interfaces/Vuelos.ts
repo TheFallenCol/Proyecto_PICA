cb4c132cbee1432c950ec985cc7379cb65334498
TheFallenCol <germansilva1944@outlook.com>
2020-11-28 17:33:20 -0500

    Add Vuelos interface

---
+++ WebApplication-SPA/src/app/interfaces/Vuelos.ts
@@ -0,0 +1,11 @@
+export interface Vuelos{
+    Supplier: string;
+    OriginAirport : string;
+    DestinationAirport:string;
+    Origin : string;
+    Destination:string;
+    StartDate: Date;
+    EndDate: Date;
+    Price: number;
+    FlightCode: string;   
+}
