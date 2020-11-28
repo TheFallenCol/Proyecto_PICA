65f318a598d306ce3499517cb421aeb6d2ead587
TheFallenCol <germansilva1944@outlook.com>
2020-11-28 18:10:40 -0500

    Add components and module stepper

--- WebApplication-SPA/src/app/vuelos/vuelos.module.ts
+++ WebApplication-SPA/src/app/vuelos/vuelos.module.ts
@@ -5,11 +5,14 @@ import { CommonModule } from '@angular/common';
 import { VuelosRoutingModule } from './vuelos-routing.module';
 import { ConsultaVuelosComponent } from './consulta-vuelos/consulta-vuelos.component';
 import { ReservaVuelosComponent } from './reserva-vuelos/reserva-vuelos.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule} from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FlightCardComponent } from './flight-card/flight-card.component';
 
@@ -20,10 +23,13 @@ import { FlightCardComponent } from './flight-card/flight-card.component';
     FlightCardComponent
   ],
   imports: [
+    MatStepperModule,
+    MatButtonModule,
     MatNativeDateModule,
     MatDatepickerModule,
     MatInputModule,
     MatFormFieldModule,
+    MatCardModule,
     MatSelectModule,
     MatAutocompleteModule,
     ReactiveFormsModule,
@@ -31,7 +37,8 @@ import { FlightCardComponent } from './flight-card/flight-card.component';
     VuelosRoutingModule
   ],
   exports: [
-    ConsultaVuelosComponent
+    ConsultaVuelosComponent,
+    FlightCardComponent
   ]
 })
 export class VuelosModule { }
