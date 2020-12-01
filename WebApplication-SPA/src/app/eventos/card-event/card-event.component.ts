46dc8bfa5d40dd97134087c533fceed414be9185
TheFallenCol <germansilva1944@hotmail.com>
2020-11-30 21:26:31 -0500

    Load events from service

--- WebApplication-SPA/src/app/eventos/card-event/card-event.component.ts
+++ WebApplication-SPA/src/app/eventos/card-event/card-event.component.ts
@@ -14,67 +14,19 @@ export class CardEventComponent implements OnInit {
   selectedEvent:TourEvent;
   loadingElement:boolean=false;
 
-  tourEvents: TourEvent[] = [
-    {
-      tourEventId:1, 
-      shortDescription: 'Pasto - Santa Fe', 
-      description: 'Cuartos de Final - Liga Betplay I 2020', 
-      eventDate: new Date('2020-11-22'),
-      eventCity:'Pasto[PSO]',
-      imgLocal:'Pasto.png',
-      imgVisitante:'Santafe.png',
-      value: 50000
-    },
-    {
-      tourEventId:2, 
-      shortDescription: 'Junior - Tolima', 
-      description: 'Cuartos de Final - Liga Betplay I 2020', 
-      eventDate: new Date('2020-11-22'),
-      eventCity:'Barranquilla[BAQ]',
-      imgLocal:'Junior.png',
-      imgVisitante:'Tolima.png',
-      value: 50000
-    },
-    {
-      tourEventId:3, 
-      shortDescription: 'La Equidad - Deportivo Cali', 
-      description: 'Cuartos de Final - Liga Betplay I 2020', 
-      eventDate: new Date('2020-11-21'),
-      eventCity:'Bogota[BOG]',
-      imgLocal:'Equidad.png',
-      imgVisitante:'Cali.png',
-      value: 50000
-    },
-    {
-      tourEventId:4, 
-      shortDescription: 'America - A. Nacional', 
-      description: 'Cuartos de Final - Liga Betplay I 2020', 
-      eventDate: new Date('2020-11-21'),
-      eventCity:'Cali[CAL]',
-      imgLocal:'America.png',
-      imgVisitante:'Nacional.png',
-      value: 50000
-    },
-    {
-      tourEventId:5, 
-      shortDescription: 'Santa Fe - Pasto', 
-      description: 'Cuartos de Final - Liga Betplay I 2020', 
-      eventDate: new Date('2020-11-28'),
-      eventCity:'Bogota[BOG]',
-      imgLocal:'Santafe.png',
-      imgVisitante:'Pasto.png',
-      value: 50000
-    }
-  ];
+  tourEvents: TourEvent[];
   
-  constructor(private eventosService: EventosService) { }
+  constructor(private eventosService: EventosService) {
+  }
 
   ngOnInit(): void {
-    this.loadingElement = true;    
-    setTimeout(() => { 
-      this.loadingElement = false;
-    }, 2000);
-    console.log('Buscar eventos por ciudad ->',this.citySearch);
+    this.loadingElement = true;
+    this.eventosService.getEventByCity(this.citySearch)
+    .subscribe(response => {
+      this.tourEvents = [];
+      this.tourEvents = response;
+      this.loadingElement = false;            
+    });
   }
 
   selectingEvent(selectedEvent:TourEvent){
