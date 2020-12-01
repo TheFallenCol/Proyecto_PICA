d82116c17229a180496178af1057870e708153b9
TheFallenCol <germansilva1944@hotmail.com>
2020-11-30 21:28:02 -0500

    Add sidenav module

--- WebApplication-SPA/src/app/hotel/hotel.module.ts
+++ WebApplication-SPA/src/app/hotel/hotel.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatCardModule,
     MatSelectModule,
     MatAutocompleteModule,
+    MatSidenavModule,
     ReactiveFormsModule
   ],
   exports:[
